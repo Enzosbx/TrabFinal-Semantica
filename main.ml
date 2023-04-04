b122b39fc7159772759d6a20fcc4765caa69fae5
Enzo Sergi B. Xavier <91389519+Enzosbx@users.noreply.github.com>
2023-04-04 20:22:29 -0300

    Update main.ml

--- main.ml
+++ main.ml
@@ -1,6 +1,21 @@
+
+(* Inicialmente definimos os types do Ocaml que vão ser utlizado. 
+A gente separou todos os operadores em um type separado chamado operator. 
+  Além dos operadores que estavam na especificação do trabalho, eu implementei o Nig (Não igual) *)
+
+
+
 type operator = Soma | Subt| Mult | Div | Ig | IgMen | Men | IgMai | Mai | Or | And | Nig
+                
+ (* Após isso definimos o identificador ident, que será um string, e os tipos da linguagem L1 propriamente ditos (Int, Bool, Ident, Fn, List ….) *)                
 
 type ident = string (* identificador, variavel x *)
+  
+                    
+    (* Depois temos as expressões da linguagem L1. 
+   Conseguimos implementar todas, com exceção das 4 últimas (Match Nil, Just, Nothing, MatchNoth,Just) e do Pair. 
+   Além disso, incluímos duas expressões mais simples, o Not e o IsEmpty *)
+
   
 type tipo  = TyInt | TyBool | TyIdent of ident | TyFn of tipo * tipo | TyList of tipo | TyPair of tipo * tipo | TyMaybe of tipo
                                                  (*  T1 --> T2 *)      (* T list *)       (* T1 * T2 *)        (* maybe T *)
@@ -34,7 +49,12 @@ type expr = Num of int
                                                          
                             
               (* As últimas 4 expressões não foram implementadas *)                                       
-                                                         
+  
+(*   Para o avaliador big step, que retorna um valor, utilizamos os valores acima como possíveis resultados. 
+     Vclos representa o resultado de uma avaliação da expressão App, que sabemos que (segundo a linguagem L1) avalia para um closure, com o identificador (ident), corpo (expr) e ambiente (env). 
+     E Vrclos é o closure para funções recursivas (Let Rec), em que o primeiro ident é o nome da função recursiva, 
+     o segundo ident é o argumento da função anônima associada à função recursiva, expr é o corpo dessa função anônima e env é o ambiente da declaração da função recursiva. *)
+                               (* Além disso temos  o valor RRaise, que é o resultado para expressões em que alguma exceção deve ser acionada.   *)                                               
                                                          
 type value = Vnum of int
            | Vbool of bool
@@ -45,7 +65,10 @@ type value = Vnum of int
            | Vclos of ident * expr * env
            | Vrclos of ident * ident * expr * env       
            | RRaise             
-                    
+ 
+  
+(* Por fim , o ambiente é uma lista de pares identificador - tipo. *)
+                   
 
 and env = (ident * value) list  (* ambiente é uma lista de pares identificador - tipo *)
   
@@ -55,35 +78,54 @@ and env = (ident * value) list  (* ambiente é uma lista de pares identificador 
 (* ------------------------------ *)
 (* INICIO DAS FUNÇÕES AUXILIARES TYPE INFER *)
 (* ------------------------------ *)
-(* ------------------------------ *)                               
+(* ------------------------------ *)           
+
+
+    (* C = Conjuntos de equações de tipo *)
+    (* T é um tipo, com tlvz variaveis de tipo *)
+    (* e é uma expressão da linguagem L1 *)
+    (* env é um ambiente de tipo mapeando variáveis declaradas para seus tipos *)
         
 
-    (* CONSTRAINTS *)                        
+    (* CONSTRAINTS --> Restrições de tipo *)   
+
+    (* Basicamente, são pares (T1:tipo, T2:tipo), em que T1 é o tipo inferido e T2 o esperado.
+   É necessário que, para que a inferência seja correta, T1 = T2. Temos também os Sets, que são conjuntos de TypeConstraints *)
   
   
    
    (* um par (T1, T2) representa T1 = T2, onde T1 é considerado o tipo inferido
 (actual) e T2 o tipo esperado (expected). *) 
+
 type typeConstraint = tipo * tipo 
 type typeConstraintSet = typeConstraint list (* conjunto de type constraints *)
              
-                                             
-       
-                                             
   
-   (* COLETA DE EQUACOES DE TIPO *)                                     
                                              
-                                             
-                                                                             
-type typeEnv = (ident * tipo) list
+   (* ETAPA 1 - COLETA DE EQUACOES DE TIPO *) 
+
+    
+
+   (* Basicamente, percorremos a AST de entrada para obter conjuntos de equações de tipo, que representam os constraints acima. (restrições). 
+   Após isso, a gente precisa resolver as equações de tipo. *)
+
+    (* os constrainsts serão tratados na função Unify *)
 
   
-exception UndefinedIdentifier of ident
+type typeEnv = (ident * tipo) list 
 
-(* Retorna o valor associado a uma chave em uma lista de pares (chave, valor) *)
-
-let lookup = List.assoc
   
+exception UndefinedIdentifier of ident  (* Temos como erro/exceção na etapa 1 “Undefined Identifier”, que é acionada caso, na hora de percorrermos a AST, exista um identificador não-declarado. *)
+
+
+let lookup = List.assoc  (* Função lookup (environment) usada nos testes: retorna o valor associado a uma chave em uma lista de pares (chave, valor) *)
+                          (* ex: em um ambiente, com Vnum(7), retorna que é o valor 7 do tipo Vnum*)
+  
+               
+    (* A função collectTyEqs É basicamente uma função que, dentro dela, tem uma recursiva collect, que faz o pattern matching entre as expressões e seus tipos *)
+   
+   
+    (* A função collect retorna um conjunto C de equações de tipo e um tipo T *)
                
 let collectTyEqs env expr = let varCount = ref (-1) in
   
@@ -96,15 +138,16 @@ let collectTyEqs env expr = let varCount = ref (-1) in
     
     (* C-Num *)
     | Num (_) ->
-        (TyInt, [])
-    
+        (TyInt, [])                     (* Abaixo, estão todas as regras de coleta de equações de tipo, como especificadas nos slides *)
+                        (* Por exemplo, Num é TyInt, Bool é TyBool, as operações 'Oper' vão depender dos operadores e1 e e2 (se é operação Int x Int -> Int ou     
+                                      Int x Int --> Bool, etc.... *)
     (* C-Bool *)
     | Bool (_) ->
         (TyBool, [])
   
     (* C-<Oper>, onde <Oper> é uma operação Int x Int -> Int *)
-    | Oper ((Soma | Subt | Mult | Div), e1, e2) ->
-        let (t1, c1) = collect env e1 in
+    | Oper ((Soma | Subt | Mult | Div), e1, e2) ->          
+        let (t1, c1) = collect env e1 in          
         let (t2, c2) = collect env e2 in
         (TyInt, c1 @ c2 @ [(t1, TyInt); (t2, TyInt)])
     
@@ -204,22 +247,30 @@ let collectTyEqs env expr = let varCount = ref (-1) in
         
   
   in
-  collect env expr
+  collect env expr     
     
     
-    
-    (* ALGORITMO DE SUBSTITUIÇÃO DE VARIAVEIS DE TIPO *)
-    
-type substMapping  = ident * tipo
-type substituition = substMapping list
+    (* ETAPA 2 - DESMEMBRADA DO UNIFY DOS SLIDES *)
 
-let rec applySubstMapping (x, tSub) t = match t with
-  | TyIdent y when y = x -> tSub
+    (* Objetivo - solucionar o conjunto C de equações de tipos - representadas por uma lista de pares de tipos - da etapa 1 *)
+  
+  
+    (* ETAPA 2.1 - ALGORITMO DE SUBSTITUIÇÃO DE VARIAVEIS DE TIPO *)
+
+
+     (* objetivo: Obter substituições de tipo para equações de tipo coletadas na etapa 1, para permitir a resolução na Unify prop.dita *)
+
+
+type substMapping  = ident * tipo                
+type substituition = substMapping list             
+
+let rec applySubstMapping (x, tSub) t = match t with     (* Temos aqui também uma função recursiva que realiza a substituição de tipos, seguindo a regra dos slides *)
+  | TyIdent y when y = x -> tSub                           (* É basicamente um mapeamento de variaveis de tipos para tipos *)
   | TyIdent y            -> TyIdent y
-  | TyInt              -> TyInt
+  | TyInt              -> TyInt         
   | TyBool             -> TyBool
   | TyFn (t1, t2)      -> TyFn (applySubstMapping (x, tSub) t1,
-                                applySubstMapping (x, tSub) t2)
+                                applySubstMapping (x, tSub) t2)     
   | TyList t           -> TyList (applySubstMapping (x, tSub) t)
 
 let rec applySubs subs t = match subs with
@@ -229,13 +280,19 @@ let rec applySubs subs t = match subs with
 
 
 
-
-    (* UNIFY *)
+    (* ETAPA 2.2 - UNIFY *)           (* Por fim, temos a função propriamente dita Unify, que une os resultados da etapa 1 e da etapa 2.1, ou seja, pega a substituição de tipo da etapa 2, e o tipo
+                                  T produzido na etapa 1, e aplica a substituição de tipo para esse tipo T' final do programa *)
+                      
   
 exception OccursCheckError of ident * tipo
 exception UnificationError of typeConstraint
+                                                         
+let unify eqs =                                    (* Há casos em que não é possivel satisfazer todas as restrições de tipo coletadas,
+                                                    aí programa submetido ao typeInfer é portanto, considerado mal tipado (aciona exceções acima ) *)
+                                                       
+                                                 (*   A unificação, se não falhar, retorna a substituição mais geral que
+                                                         torna verdadeira todos as equações de tipo C *)
 
-let unify eqs =
   let rec occurs x t = match t with
     | TyIdent y             -> y == x
     | (TyInt | TyBool)    -> false
@@ -243,10 +300,10 @@ let unify eqs =
     | TyList t            -> occurs x t
   in
   
-  let rec replaceInType x tSub t = applySubstMapping (x, tSub) t
+  let rec replaceInType x tSub t = applySubstMapping (x, tSub) t   (* chama as substituições de tipo *)
   in
   
-  let rec replace x t constraints = match constraints with
+  let rec replace x t constraints = match constraints with          (* utiliza os constraints definidos mais acima *)
     | [] -> []
     | (t1, t2) :: c -> (replaceInType x t t1,
                         replaceInType x t t2) :: (replace x t c)
@@ -277,7 +334,7 @@ let unify eqs =
     | (TyList t1, TyList t2) :: c ->
         solve subst ((t1, t2) :: c)
     
-    | (actualType, expectedType) :: c ->
+    | (actualType, expectedType) :: c ->                         (* se der erro na unificação, raise *)
         raise (UnificationError (actualType, expectedType))
   in
   solve [] eqs   
@@ -297,8 +354,8 @@ let unify eqs =
 
 (* TYPE INFER *)
                                    
-let typeInfer env expr =
-  let (t, c) = collectTyEqs env expr in
+let typeInfer env expr =                  (* Type Infer, que chama às substituições de tipo (etapa 1) e depois o unify (etapa 2 *)
+  let (t, c) = collectTyEqs env expr in   
   let subs = unify c in
   applySubs subs t
     
@@ -306,19 +363,19 @@ let typeInfer env expr =
 
 
 
-    (*  AVALIADOR BIG STEP *)
+    (*  AVALIADOR BIG STEP *)      (* Utilizamos big step com ambientes *)
 
 
 (* Excecao a ser ativada quando nenhuma regra se aplica. *)
-exception NoRuleApplies
-
+exception NoRuleApplies 
 exception UndefinedIdentifier of ident
 
-let remove_tuple var list =
+let remove_tuple var list =                            (* No avaliador big step, utilizamos ambientes que guardam as substituições *)
+                                                        (* Um ambiente é um mapeamento de variáveis para valores *)
   List.filter (fun (k,_) -> k <> var) list
 
-
-let update_env var v1 env : env = match env with
+ 
+let update_env var v1 env : env = match env with  
     [] -> [(var,v1)]
   | hd::tl ->
       if (List.exists (fun (k,_) -> k = var) env)
@@ -330,9 +387,9 @@ let lookup_environment = List.assoc
 
 let empty_env : env = []
 
-let rec eval (env:env) (exp : expr) : value =	match exp with
+let rec eval (env:env) (exp : expr) : value =	match exp with      (* Aqui temos a função recursiva eval, que avalia segundo as regras da big step utilizadas nos slides *)
 
-  (* Valores *)
+  (* Valores *)                                              (* Cabe relembrar que funções avaliam para closures - identificador, corpo, ambiente, como iremos ver nos exemplos *)
     Num(n) -> Vnum(n)
   | Bool(b) -> Vbool(b)
 
@@ -478,6 +535,15 @@ let environment = empty_env;;
 	(* Testes - cada um corresponde à uma regra da semântica BIG-STEP *)
 
 
+let progFat =
+  LetRec ("fat", TyInt, TyInt, "n", TyInt, 
+          IfElse (Oper (Ig, VarIdent "n", Num 0),
+                  Num 1,
+                  Oper(Mult, VarIdent "n", App (VarIdent "fat", Oper (Subt, VarIdent "n", Num 1)))),
+          App (VarIdent "fat", Num 3))
+    
+    
+
 (* BS-NUM *)
 let numEx1 = Num(7);;
 let evaln = eval environment numEx1;;
@@ -516,6 +582,9 @@ let env = update_env "numAccept" (Vnum(7)) environment;;
 lookup_environment "numAccept" env;;
 
 
+let env = update_env "numAccept" (Vnum(9)) environment;;
+lookup_environment "numAccept" env;;
+
 (** Teste Let Rec com fatorial **)
 
 let fat =
@@ -526,9 +595,7 @@ let fat =
           VarIdent "fat");;
 
 let progFat = App (fat, Num 3);; 
-let evalpf = eval environment progFat;;
 
-    
   
 (** Teste Let Rec com RAISE e com fatorial **)
 
@@ -757,6 +824,3 @@ eval empty_env exp;;
 (* BS-TLRS *)
 let exp = Tl(Raise);;
 eval empty_env exp;;
-
-
-
